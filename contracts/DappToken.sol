262326247b1fe2085487bd063cfdac6b1d31a1c1
Shaharbaz203 <64959721+ShahArbaz203@users.noreply.github.com>
2021-01-20 02:58:44 +0400

    Approved tokens
     approves tokens for delegated transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,15 +7,65 @@ contract DappToken{
     //Symbol
  string public symbol = "DAPP";
  string public standard = "DApp Token v1.0";
+  uint256 public totalSupply;
+
+
+  
+event Transfer(
+address indexed _from,
+address indexed _to,
+uint256 _value
+);
+
+ //transfer event
+ event Approval(
+address indexed _owner,
+address indexed _spender,
+uint256 indexed _value
+ );
 
     mapping(address => uint256) public balanceOf;
-
-    uint256 public totalSupply;
+    mapping(address => mapping(address => uint256)) public allowance;
+  //allowance
+  
     
     constructor (uint256 _initialSupply){
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         //allocate the initial supply
     }
+    //Transfer
+    
+     
+    function transfer(address _to, uint256 _value) public   returns (bool  success)
+    {
 
+
+        
+
+
+       require( balanceOf[msg.sender] >= _value);
+       //Transfer the balance
+
+       balanceOf[msg.sender] -=_value;
+       balanceOf[_to] +=_value;
+       emit Transfer(msg.sender,_to,_value);
+       return true;
+    
+    }
+
+   //approve
+
+   function approve(address _spender, uint256 _value) public returns (bool success){
+       //allowance
+  allowance[msg.sender][_spender] = _value; 
+     //Approve event
+     
+      Approval(msg.sender, _spender, _value);
+
+
+      return true;
+       
+   }
+   //thansfer from
 }
